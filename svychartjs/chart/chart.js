2f0a934987dc645d2b5b1c711ac826db02a3df4e
Tuan Nguyen <tuanway@gmail.com>
2017-04-19 07:16:04 -0700

    add min.js version of chart lib remove on window resizing action

--- svychartjs/chart/chart.js
+++ svychartjs/chart/chart.js
@@ -206,30 +206,6 @@ angular.module('svychartjsChart', ['servoy']).directive('svychartjsChart', funct
 
             }
 
-            //when window is resized redraw the chart
-
-            var rtime;
-            var timeout = false;
-            var delta = 200;
-            $(window).resize(function() {
-                rtime = new Date();
-                if (timeout === false) {
-                    timeout = true;
-                    setTimeout(resizeend, delta);
-                }
-            });
-
-            function resizeend() {
-                if (new Date() - rtime < delta) {
-                    setTimeout(resizeend, delta);
-                } else {
-                    timeout = false;
-                    if ($scope.model.chart) {
-                        refreshChart();
-                    }
-                }
-            }
-
             //if the data model node changes redraw the chart
             $scope.$watchCollection('model.node', function(newValue, oldValue) {
                 refreshChart();
