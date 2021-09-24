461989a7735d6f907c1edb60216050e120d4c99c
Robert Ivens <robert@roclasi.com>
2021-09-24 09:45:55 +0200

    Support for global- and form methods to string
    refactored and split fnToString function into 2 functions
    - formFnToString
    - globalFnToString

--- svyChartUtils/stringUtils.js
+++ svyChartUtils/stringUtils.js
@@ -11,19 +11,21 @@ function replaceAll(str, search, replacement) {
 ;
 
 /**
- * @param {String} scope
- * @param {String} fn
- * Convert a function's body into string
+ * @private
+ *
+ * @param {JSMethod} fn
+ * @return {String}
+ *
  * @properties={typeid:24,uuid:"18D96B11-6C4B-4ED4-A3DF-069EEB870F67"}
  */
-function fnToString(scope, fn) {
+function fnToString(fn) {
 	/** @type {String} */
-	var s = solutionModel.getGlobalMethod(scope, fn).code.split('*/')[1]
+	var s = fn.code.split('*/')[1]
 	var b = s.slice(s.indexOf("{") + 1, s.lastIndexOf("}"));
 	b = b.split('\n')
 	var r = [];
 	for (var i = 0; i < b.length; i++) {
-		if (b[i].indexOf('//')==-1){
+		if (b[i].indexOf('//') == -1) {
 			r.push(b[i])
 		}
 	}
@@ -31,4 +33,42 @@ function fnToString(scope, fn) {
 	r = r.replace(/(\r\n|\n|\r)/gm, "");
 	r = replaceAll(r, '"', "'");
 	return r;
+}
+
+/**
+ * Convert a form function's body into string
+ *
+ * @param {String} formName
+ * @param {String} methodName
+ * @return {String}
+ *
+ * @properties={typeid:24,uuid:"D3E14A89-26DF-4946-9B17-A319A08F6C93"}
+ */
+function formFnToString(formName, methodName) {
+	var form = solutionModel.getForm(formName);
+	if (!form) {
+		throw new Error("Can't get form '" + formName + "'");
+	}
+	var fn = form.getMethod(methodName);
+	if (!fn) {
+		throw new Error("Can't get method '" + methodName + "' from form '" + formName + "'");
+	}
+	return fnToString(fn);
+}
+
+/**
+ * Convert a global function's body into string
+ *
+ * @param {String} scope
+ * @param {String} methodName
+ * @return {String}
+ *
+ * @properties={typeid:24,uuid:"8145D42A-80FE-4A07-922F-788118B09D51"}
+ */
+function globalFnToString(scope, methodName) {
+	var fn = solutionModel.getGlobalMethod(scope, methodName);
+	if (!fn) {
+		throw new Error("Can't get method '" + methodName + "' from global scope '" + scope + "'");
+	}
+	return fnToString(fn);
 }
