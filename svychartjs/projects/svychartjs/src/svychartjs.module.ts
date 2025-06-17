5d0936638fbeb00b2d10e1fab6f03ac5acb48f66
Johan Compagner <jcompagner@servoy.com>
2025-06-17 16:49:55 +0200

    SVY-20291 2025.06RC - chartJS breaks when using time scale
    you now need to register the default stuff

--- svychartjs/projects/svychartjs/src/svychartjs.module.ts
+++ svychartjs/projects/svychartjs/src/svychartjs.module.ts
@@ -1,14 +1,16 @@
 
 import { NgModule } from '@angular/core';
 import {SvyChartJS} from './chart/chart';
-import { BaseChartDirective } from 'ng2-charts';
+import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { CommonModule } from '@angular/common';
  
 @NgModule({
     declarations: [
         SvyChartJS
     ],
-    providers: [],
+    providers: [
+        provideCharts(withDefaultRegisterables())
+    ],
     imports: [
         BaseChartDirective,
         CommonModule 
